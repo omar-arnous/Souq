e00665d1309e5dcc7730873786fb898effac2332
omar-arnous <omar99.arnous@gmail.com>
2023-06-10 08:28:55 +0300

    Implementing the grid view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:souq/models/product.dart';
 import 'package:souq/providers/product_provider.dart';
 
-class HomeScreen extends ConsumerWidget {
+late Future<List<Product>> _value;
+
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _value = ref.read(productProvider.notifier).getProducts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final products = ref.watch(productProvider);
+
     return Container(
       margin: const EdgeInsets.all(20),
       child: FutureBuilder(
-        future: ref.read(productProvider.notifier).getProducts(),
-        builder: (context, snapshot) {
+        future: _value,
+        builder: (context, AsyncSnapshot<List<Product>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
               child: CircularProgressIndicator(
@@ -20,22 +35,24 @@ class HomeScreen extends ConsumerWidget {
               ),
             );
           }
-          return Column(
-            children: [
-              Text(
-                'Products',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              GridView.builder(
-                itemCount: products.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                ),
-                itemBuilder: (context, i) => Text(products[i].title),
-              ),
-            ],
-          );
+          // return Column(
+          //   children: [
+          //     Text(
+          //       'Products',
+          //       style: Theme.of(context).textTheme.titleLarge,
+          //     ),
+          //     GridView.builder(
+          //       itemCount: products.length,
+          //       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          //         crossAxisCount: 2,
+          //         childAspectRatio: 3 / 2,
+          //         crossAxisSpacing: 20,
+          //         mainAxisExtent: 20,
+          //       ),
+          //       itemBuilder: (context, i) => Text(products[i].title),
+          //     ),
+          //   ],
+          // );
         },
       ),
     );
