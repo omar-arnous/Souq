392a17b5abf12d93065d924a73cfe80f65494b2d
omar-arnous <omar99.arnous@gmail.com>
2023-06-13 20:15:02 +0300

    refactoring

--- lib/providers/product_provider.dart
+++ lib/providers/product_provider.dart
@@ -7,7 +7,6 @@ class ProductsNotifier extends StateNotifier<List<Product>> {
   ProductsNotifier() : super([]);
 
   Future<List<Product>> getProducts() async {
-    print("Getting Products");
     final response = await ApiClient.getProducts();
     final products = response.data['products'];
     for (final product in products) {
