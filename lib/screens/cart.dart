77c08f5e1bfba993a0e67c58163154622022d2f4
omar-arnous <omar99.arnous@gmail.com>
2023-06-13 18:54:19 +0300

    implement cart item widget

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:souq/providers/cart_provider.dart';
+import 'package:souq/widgets/cart_item.dart';
 
 class CartScreen extends ConsumerWidget {
   const CartScreen({super.key});
@@ -19,7 +20,10 @@ class CartScreen extends ConsumerWidget {
     }
 
     return ListView.builder(
-        itemCount: cart.length,
-        itemBuilder: (context, i) => Text(cart[i].title));
+      itemCount: cart.length,
+      itemBuilder: (context, i) => CartItem(
+        cart[i],
+      ),
+    );
   }
 }
---
+++ lib/widgets/cart_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:souq/providers/cart_provider.dart';
+
+import '../models/cart.dart';
+
+class CartItem extends ConsumerWidget {
+  const CartItem(this.cart, {super.key});
+  final Cart cart;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Card(
+      margin: const EdgeInsets.all(15),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  cart.title,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Text(
+                  '${cart.calcAmount()} SP',
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () => ref.read(cartProvider.notifier).addOne(cart),
+                  icon: const Icon(
+                    Icons.remove,
+                    color: Color(0xffF1F1F1),
+                  ),
+                ),
+                Text(
+                  '${cart.quantity}',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+                IconButton(
+                  onPressed: () =>
+                      ref.read(cartProvider.notifier).removeOne(cart),
+                  icon: const Icon(
+                    Icons.add,
+                    color: Color(0xffF1F1F1),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
